fab85365584032694f036489a6d244ad896b8e2d
xu.liqing <liqing.xu@enjoymove.com>
2024-09-23 15:51:17 +0800

    添加分析文件

---
+++ data_analysis.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pickle
+import matplotlib.pyplot as plt
+
+file = open('output.pkl', 'rb')
+traindata = pickle.load(file)
+np.set_printoptions(suppress=True, threshold=np.inf)
+fig, ax = plt.subplots()
+for epoch in traindata['Epochs']:
+    for index, row in enumerate(traindata['Input'][epoch]):
+        for i in range(len(row)):
+            for j in range(len(row[i])):
+              if(row[i][j] == 1 or row[i][j] == 2):
+                  print(traindata['Output'][epoch][0][i][j])
+                  ax.plot( traindata['Output'][epoch][0][i][j])
+plt.show()
