51cb50f21ae02d1a0f2d887d07adb52913d242eb
xu.liqing <liqing.xu@enjoymove.com>
2025-02-05 18:30:46 +0800

    整理格式

--- model.py
+++ model.py
@@ -8,30 +8,41 @@ from corpus import TranslationCorpus
 class ScaledDotProductionAttention(nn.Module):
     def __init__(self):
         super(ScaledDotProductionAttention,self).__init__()
+        
     def forward(self, Q, K, V, mask, model_config):
         scores = torch.matmul(Q, K.transpose(-1,-2))/np.sqrt(model_config['d_k'])
         scores.masked_fill_(mask, -1e9)
         weights = nn.Softmax(dim = -1)(scores)
         contexts = torch.matmul(weights, V)
         return contexts, weights
-
 class MultiHeadAttention(nn.Module):
     def __init__(self, model_config):
         super(MultiHeadAttention,self).__init__()
-        self.W_Q = nn.Linear(model_config['d_embedding'], model_config['n_heads']*model_config['d_k'])
-        self.W_K = nn.Linear(model_config['d_embedding'], model_config['n_heads']*model_config['d_k'])
-        self.W_V = nn.Linear(model_config['d_embedding'], model_config['n_heads']*model_config['d_v'])
-        self.Linear = nn.Linear(model_config['n_heads']*model_config['d_v'], model_config['d_embedding'])
+        self.W_Q = nn.Linear(model_config['d_embedding'], 
+                             model_config['n_heads'] * model_config['d_k'])
+        self.W_K = nn.Linear(model_config['d_embedding'], 
+                             model_config['n_heads'] * model_config['d_k'])
+        self.W_V = nn.Linear(model_config['d_embedding'], 
+                             model_config['n_heads'] * model_config['d_v'])
+        self.Linear = nn.Linear(model_config['n_heads'] * model_config['d_v'], 
+                                model_config['d_embedding'])
         self.layer_norm = nn.LayerNorm(model_config['d_embedding'])
 
     def forward(self,Q,K,V,attn_mask, model_config):
         residual, batch_size = Q, Q.size(0)
-        q_s = self.W_Q(Q).view(batch_size, -1, model_config['n_heads'], model_config['d_k']).transpose(1,2)
-        k_s = self.W_K(K).view(batch_size, -1, model_config['n_heads'], model_config['d_k']).transpose(1,2)
-        v_s = self.W_V(V).view(batch_size, -1, model_config['n_heads'], model_config['d_v']).transpose(1,2)
-        attn_mask = attn_mask.unsqueeze(1).repeat(1, model_config['n_heads'], 1, 1)
-        context, weights = ScaledDotProductionAttention()( q_s, k_s, v_s, attn_mask, model_config)
-        context = context.transpose(1,2).contiguous().view(batch_size, -1, model_config['n_heads']*model_config['d_v'])
+        q_s = self.W_Q(Q).view(batch_size, -1, model_config['n_heads'], 
+                               model_config['d_k']).transpose(1,2)
+        k_s = self.W_K(K).view(batch_size, -1, model_config['n_heads'], 
+                               model_config['d_k']).transpose(1,2)
+        v_s = self.W_V(V).view(batch_size, -1, model_config['n_heads'], 
+                               model_config['d_v']).transpose(1,2)
+        attn_mask = attn_mask.unsqueeze(1).repeat(1, 
+                                                  model_config['n_heads'], 1, 1)
+        context, weights = ScaledDotProductionAttention()(q_s, k_s, v_s, 
+                                                          attn_mask, 
+                                                          model_config)
+        context = context.transpose(1,2).contiguous().view(batch_size, -1, 
+                                    model_config['n_heads']*model_config['d_v'])
         output = self.Linear(context)
         output = self.layer_norm(output + residual)
         return output, weights
@@ -39,9 +50,13 @@ class MultiHeadAttention(nn.Module):
 class PoswiseFeedForwardNet(nn.Module):
     def __init__(self, model_config, d_ff=2048):
         super(PoswiseFeedForwardNet,self).__init__()
-        self.conv1 = nn.Conv1d(in_channels=model_config['d_embedding'], out_channels=d_ff, kernel_size=1)
-        self.conv2 = nn.Conv1d(in_channels=d_ff, out_channels=model_config['d_embedding'], kernel_size=1)
+        self.conv1 = nn.Conv1d(in_channels = model_config['d_embedding'], 
+                               out_channels = d_ff, kernel_size=1)
+        self.conv2 = nn.Conv1d(in_channels = d_ff,
+                               out_channels = model_config['d_embedding'],
+                               kernel_size=1)
         self.layer_norm = nn.LayerNorm(model_config['d_embedding'])
+        
     def forward(self, inputs):
         residual = inputs
         output = nn.ReLU()(self.conv1(inputs.transpose(1,2)))
@@ -73,6 +88,7 @@ class Encoder(nn.Module):
                                     model_config['d_embedding']),freeze = True)
         self.layers = nn.ModuleList(EncoderLayer(model_config) 
                                     for _ in range(model_config['n_layers']))
+        
     def forward(self, enc_inputs, model_config):
         pos_indices = torch.arange(1,
                                    enc_inputs.size(1) + 1).\
@@ -93,6 +109,7 @@ class DecoderLayer(nn.Module):
         self.dec_self_attn = MultiHeadAttention(model_config)
         self.dec_enc_attn = MultiHeadAttention(model_config)
         self.pos_ffn = PoswiseFeedForwardNet(model_config)
+        
     def forward(self, dec_inputs, enc_outputs, dec_self_attn_mask, 
                 dec_enc_attn_mask, model_config):
         dec_outputs, dec_self_attn = self.dec_self_attn(dec_inputs, 
@@ -117,17 +134,24 @@ class Decoder(nn.Module):
         self.pos_emb = nn.Embedding.from_pretrained(
                                     get_sin_enc_table(corpus.tgt_len+1, 
                                     model_config['d_embedding']),freeze=True)
-        self.layers = nn.ModuleList([DecoderLayer(model_config) for _ in range(model_config['n_layers'])])
+        self.layers = nn.ModuleList([DecoderLayer(model_config) 
+                                     for _ in range(model_config['n_layers'])])
+        
     def forward(self,dec_inputs, enc_inputs, enc_outputs, model_config):
         pos_indices = torch.arange(1, dec_inputs.size(1) + 1).unsqueeze(0).to(dec_inputs)
         dec_outputs = self.tgt_emb(dec_inputs) + self.pos_emb(pos_indices)
         dec_self_attn_pad_mask = get_attn_pad_mask(dec_inputs,dec_inputs)
         dec_self_attn_subsequent_mask = get_attn_subsequent_mask(dec_inputs)
-        dec_self_attn_mask = torch.gt((dec_self_attn_pad_mask + dec_self_attn_subsequent_mask),0)
+        dec_self_attn_mask = torch.gt((dec_self_attn_pad_mask + 
+                                       dec_self_attn_subsequent_mask),0)
         dec_enc_attn_mask = get_attn_pad_mask(dec_inputs, enc_inputs)
         dec_self_attns,dec_enc_attns = [],[]
         for layer in self.layers:
-            dec_outputs, dec_self_attn, dec_enc_attn = layer(dec_outputs, enc_outputs, dec_self_attn_mask, dec_enc_attn_mask, model_config)
+            dec_outputs, dec_self_attn, dec_enc_attn = layer(dec_outputs, 
+                                                             enc_outputs, 
+                                                             dec_self_attn_mask, 
+                                                             dec_enc_attn_mask, 
+                                                             model_config)
             dec_self_attns.append(dec_self_attn)
             dec_enc_attns.append(dec_enc_attn)
             #Visualization.PlotOneDimTensor(dec_outputs)
@@ -138,11 +162,15 @@ class Transformer(nn.Module):
         super(Transformer, self).__init__()
         self.encoder = Encoder(corpus, model_config)
         self.decoder = Decoder(corpus, model_config)
-        self.projection = nn.Linear(model_config['d_embedding'], len(corpus.tgt_vocab),bias = False)
+        self.projection = nn.Linear(model_config['d_embedding'], 
+                                    len(corpus.tgt_vocab),bias = False)
         
     def forward(self, enc_inputs, dec_inputs, model_config):
         enc_outputs, enc_self_attns = self.encoder(enc_inputs, model_config)
-        dec_outputs, dec_self_attns, dec_enc_attns = self.decoder(dec_inputs, enc_inputs, enc_outputs, model_config)
+        dec_outputs, dec_self_attns, dec_enc_attns = self.decoder(dec_inputs, 
+                                                                  enc_inputs, 
+                                                                  enc_outputs, 
+                                                                  model_config)
         dec_logits = self.projection(dec_outputs)
         return dec_logits, enc_self_attns, dec_self_attns, dec_enc_attns
 def forward_hook(module, input, output):
@@ -206,7 +234,10 @@ def greedy_decoder(model, enc_input, start_symbol, model_config, tgt_len):
     next_symbol = start_symbol
     for i in range(0,tgt_len):
         dec_input[0][i] = next_symbol
-        dec_output,_,_ = model.decoder(dec_input, enc_input, enc_outputs, model_config)
+        dec_output,_,_ = model.decoder(dec_input, 
+                                       enc_input, 
+                                       enc_outputs, 
+                                       model_config)
         projected = model.projection(dec_output)
         prob = projected.squeeze(0).max(dim = -1, keepdim = False)[1]
         next_word = prob.data[i]
