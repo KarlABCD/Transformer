bb69baa68bdc8e019b4b23fc8f8dce9508ca2512
xu.liqing <liqing.xu@enjoymove.com>
2025-02-06 09:47:00 +0800

    添加config文件夹

---
+++ config/train_params.py
@@ -0,0 +1,28 @@
+# 语料相关
+data_dir = 'corpus'
+data_name = 'corpus_data.txt'
+
+# 训练相关
+epochs = 100
+device_type = 'cpu' # cpu or gpu
+dtype = 'bfloat16'
+learningrate = 0.0001
+# 模型相关的参数
+model_config = {'d_k': 64,
+                'd_v': 64,
+                'd_embedding': 128,
+                'n_heads': 1,
+                'batch_size' : 6,
+                'n_layers': 1}
+# 保存格式相关的
+bDataRecord = True
+ModelOutDir = 'checkpoint'
+ModelName = 'checkpoint.pt'
+RecordOutDir = 'modelrecord'
+RecordName = 'record.pkl'
+WorkMode = 'PreTrained'
+#WorkMode = 'scratch'
+CheckPtNum = 10
+
+# 是否需要打印重要信息
+bDebugInfo = False
